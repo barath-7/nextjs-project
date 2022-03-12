5e3e946d8c83a50ebbd4ac0c90a87337d6e65179
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-03-13 00:06:55 +0530

    Initial project setup

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList'
+const DUMMY_MEETUP = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Paris_Opera_full_frontal_architecture%2C_May_2009_%28cropped%29.jpg/1200px-Paris_Opera_full_frontal_architecture%2C_May_2009_%28cropped%29.jpg',
+    address: "Paris 53",
+    description: "This is the first meetup - Paris",
+  },
+
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/London_Parliament_2007-1.jpg/1920px-London_Parliament_2007-1.jpg",
+    address: "London 63",
+    description: "This is the second meetup - UK",
+  },
+];
+
+const HomePage =() =>{
+    return <MeetupList meetups={DUMMY_MEETUP} />
+}
+export default HomePage;
